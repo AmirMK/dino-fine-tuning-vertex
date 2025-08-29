4a1e2f36b0ea1862ae7edcf2786706a83fba1ce5
Amir Meimand <amirmeimand@gmail.com>
2025-08-29 10:41:08 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+REQUIRED_PACKAGES = [
+    "transformers==4.55.2",
+    "datasets==4.0.0",
+    "google-cloud-storage==2.19.0",
+    "Pillow==11.3.0",
+    "accelerate>=0.26.0",
+]
+
+setup(
+    name="trainer",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=REQUIRED_PACKAGES,
+    description="DINOv2 self-supervised fine-tuning.",
+)
