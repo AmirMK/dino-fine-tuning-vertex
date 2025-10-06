39ddb2841dc76850f1fc6c90efc69d20eaab2dbe
Amir Meimand <amirmeimand@gmail.com>
2025-10-05 22:25:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup, find_packages
 
 REQUIRED_PACKAGES = [
     "transformers==4.55.2",
-    "datasets==4.0.0",
+    "datasets>=2.19.0,<3",
     "google-cloud-storage==2.19.0",
-    "Pillow==11.3.0",
+    #"Pillow==11.3.0",
+    "pillow-simd==9.5.0.post1",
     "accelerate>=0.26.0",
+    "fsspec>=2024.6.0",
+    "gcsfs>=2024.6.0",
+    "cloudml-hypertune",
+    "webdataset"
 ]
 
 setup(
     name="trainer",
-    version="0.1.0",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIRED_PACKAGES,
